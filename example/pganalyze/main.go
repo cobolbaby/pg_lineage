9cfee27cabc7c8049f5e828ee07bb06ae7dbf449
Cobolbaby <Zhang.Xing-Long@inventec.com>
2021-12-26 18:32:14 +0800

    test: sql join 关系解析，完善工具方法

--- example/pganalyze/main.go
+++ example/pganalyze/main.go
@@ -9,7 +9,12 @@ import (
 func main() {
 
 	sql := `
-		SELECT * FROM demo.tbl1 a JOIN demo.tbl2 b ON a.id = b.tid;
+	SELECT
+    	*
+	FROM
+		demo.tbl1 a
+		JOIN demo.tbl2 b ON a.id = b.aid AND a.name = b.name
+		JOIN demo.tbl3 c ON a.id = c.aid;
 	`
 
 	result, err := pg_query.Parse(sql)
@@ -17,47 +22,163 @@ func main() {
 		panic(err)
 	}
 
-	// This will output "42"
 	for _, v := range result.Stmts[0].Stmt.GetSelectStmt().GetFromClause() {
-
-		// 优先遍历内层JOIN
-		if v.GetJoinExpr().GetLarg().GetJoinExpr() != nil {
-			// 递归调用
-		}
-		if v.GetJoinExpr().GetRarg().GetJoinExpr() != nil {
-			// 递归调用
-		}
-		// 然后遍历外层JOIN
-		if v.GetJoinExpr().GetLarg().GetRangeVar() != nil {
-			// 解析表名，列名，JOIN条件，记录别名与实体表名的对应关系
-			// 等待外层遍历完之后，先做表名替换，然后筛选出两个表都不是临时表的JOIN条件
-		}
-		if v.GetJoinExpr().GetRarg().GetRangeVar() != nil {
-			// 直接返回
-		}
-		// 将关联条件输出，可以是单个字段关联，也可以是多字段关联
-
-		fmt.Println(v.GetJoinExpr().GetLarg().GetRangeVar())
-
-		// 获取别名定义
-		tblAliasMap := make(map[string]string)
-
-		lrAlias := v.GetJoinExpr().GetLarg().GetRangeVar().GetAlias().GetAliasname()
-		lrReal := v.GetJoinExpr().GetLarg().GetRangeVar().GetSchemaname() + "." + v.GetJoinExpr().GetLarg().GetRangeVar().GetRelname()
-
-		tblAliasMap[lrAlias] = lrReal
-
-		rrAlias := v.GetJoinExpr().GetRarg().GetRangeVar().GetAlias().GetAliasname()
-		rrReal := v.GetJoinExpr().GetRarg().GetRangeVar().GetSchemaname() + "." + v.GetJoinExpr().GetLarg().GetRangeVar().GetRelname()
-
-		tblAliasMap[rrAlias] = rrReal
-
-		lfields := v.GetJoinExpr().GetQuals().GetAExpr().GetLexpr().GetColumnRef().GetFields()
-		fmt.Println(tblAliasMap[lfields[0].GetString_().GetStr()] + "." + lfields[1].GetString_().GetStr())
-
-		rfields := v.GetJoinExpr().GetQuals().GetAExpr().GetRexpr().GetColumnRef().GetFields()
-		fmt.Println(tblAliasMap[rfields[0].GetString_().GetStr()] + "." + rfields[1].GetString_().GetStr())
-
+		// fmt.Printf("%+v\n", v)
+		fmt.Printf("%+v", parseJoinClause(v, make(map[string]*Relation)))
 	}
 
+}
+
+type Relation struct {
+	RelName        string
+	Schema         string
+	Alias          string
+	Relpersistence string
+}
+
+type Column struct {
+	Schema  string
+	RelName string
+	Field   string
+}
+
+type Record struct {
+	LRelation    *Relation
+	RRelation    *Relation
+	RelationShip map[Column]Column
+	Alias        string
+	Type         string
+}
+
+// 返回左右表间的关系，所以主体有两个表，外加关系，不过如果返回的是一个子查询，理论上其也会有个别名
+func parseJoinClause(node *pg_query.Node, aliasMap map[string]*Relation) *Record {
+
+	if node.GetJoinExpr() == nil {
+		return nil
+	}
+
+	j := node.GetJoinExpr()
+	var lRelation, rRelation *Relation
+
+	// 优先遍历内层JOIN
+	if j.GetLarg().GetJoinExpr() != nil {
+		lnode := parseJoinClause(j.GetLarg(), aliasMap)
+
+		lRelation = &Relation{
+			RelName: "", // ???
+			Schema:  "", // ???
+			Alias:   lnode.Alias,
+		}
+	}
+	if j.GetRarg().GetJoinExpr() != nil {
+		rnode := parseJoinClause(j.GetRarg(), aliasMap)
+
+		rRelation = &Relation{
+			RelName: "",
+			Schema:  "",
+			Alias:   rnode.Alias,
+		}
+	}
+	if j.GetLarg().GetRangeVar() != nil {
+		lnode := j.GetLarg().GetRangeVar()
+
+		lRelation = &Relation{
+			RelName:        lnode.GetRelname(),
+			Schema:         lnode.GetSchemaname(),
+			Alias:          lnode.GetAlias().GetAliasname(),
+			Relpersistence: lnode.GetRelpersistence(),
+		}
+
+		aliasMap[lRelation.Alias] = lRelation
+	}
+	if j.GetRarg().GetRangeVar() != nil {
+		rnode := j.GetRarg().GetRangeVar()
+
+		rRelation = &Relation{
+			RelName:        rnode.GetRelname(),
+			Schema:         rnode.GetSchemaname(),
+			Alias:          rnode.GetAlias().GetAliasname(),
+			Relpersistence: rnode.GetRelpersistence(),
+		}
+
+		aliasMap[rRelation.Alias] = rRelation
+	}
+
+	relationShip := parseQuals(j, aliasMap)
+
+	r := &Record{
+		LRelation:    lRelation,
+		RRelation:    rRelation,
+		RelationShip: relationShip,
+		Type:         j.GetJointype().String(),
+		Alias:        j.GetAlias().GetAliasname(),
+	}
+
+	return r
+}
+
+func parseQuals(node *pg_query.JoinExpr, aliasMap map[string]*Relation) map[Column]Column {
+	if node.GetQuals() == nil {
+		return nil
+	}
+
+	relationShip := make(map[Column]Column)
+
+	if node.GetQuals().GetAExpr() != nil {
+		relationShip = parseAExpr(node.GetQuals().GetAExpr(), aliasMap)
+	} else if node.GetQuals().GetBoolExpr() != nil {
+		relationShip = parseBoolExpr(node.GetQuals().GetBoolExpr(), aliasMap)
+	}
+	// ...
+
+	return relationShip
+}
+
+func MergeMap(mObj ...map[Column]Column) map[Column]Column {
+	newObj := map[Column]Column{}
+	for _, m := range mObj {
+		for k, v := range m {
+			newObj[k] = v
+		}
+	}
+	return newObj
+}
+
+func parseBoolExpr(expr *pg_query.BoolExpr, aliasMap map[string]*Relation) map[Column]Column {
+	m := map[Column]Column{}
+	for _, v := range expr.GetArgs() {
+		m = MergeMap(m, parseAExpr(v.GetAExpr(), aliasMap))
+	}
+	return m
+}
+
+func parseAExpr(expr *pg_query.A_Expr, aliasMap map[string]*Relation) map[Column]Column {
+	l := expr.GetLexpr()
+	r := expr.GetRexpr()
+
+	var key, value Column
+
+	if len(l.GetColumnRef().GetFields()) == 2 {
+		rel := aliasMap[l.GetColumnRef().GetFields()[0].GetString_().GetStr()]
+
+		key = Column{
+			Schema:  rel.Schema,
+			RelName: rel.RelName,
+			Field:   l.GetColumnRef().GetFields()[1].GetString_().GetStr(),
+		}
+	}
+
+	if len(r.GetColumnRef().GetFields()) == 2 {
+		rel := aliasMap[r.GetColumnRef().GetFields()[0].GetString_().GetStr()]
+
+		value = Column{
+			Schema:  rel.Schema,
+			RelName: rel.RelName,
+			Field:   r.GetColumnRef().GetFields()[1].GetString_().GetStr(),
+		}
+	}
+
+	return map[Column]Column{
+		key: value,
+	}
 }
