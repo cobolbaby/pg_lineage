44a5e35ffa24058b406e449e58050654891f0412
Cobolbaby <Zhang.Xing-Long@inventec.com>
2021-12-27 15:29:13 +0800

    refactor: sql join 关系解析模块

--- example/pganalyze/main.go
+++ example/pganalyze/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
 	"fmt"
 
 	pg_query "github.com/pganalyze/pg_query_go/v2"
@@ -23,15 +25,21 @@ func main() {
 	}
 
 	for _, v := range result.Stmts[0].Stmt.GetSelectStmt().GetFromClause() {
-		// fmt.Printf("%+v\n", v)
-		fmt.Printf("%+v", parseJoinClause(v, make(map[string]*Relation)))
+		// fmt.Printf("GetFromClause %+v\n", v)
+
+		joinRel := parseJoinClause(v, make(map[string]*Relation))
+		// fmt.Printf("parseJoinClause res %+v\n", joinRel)
+
+		r := joinRel.GetRelationShip()
+		fmt.Printf("MergeMap res %+v\n", r)
+
 	}
 
 }
 
 type Relation struct {
-	RelName        string
 	Schema         string
+	RelName        string
 	Alias          string
 	Relpersistence string
 }
@@ -40,14 +48,32 @@ type Column struct {
 	Schema  string
 	RelName string
 	Field   string
+	Alias   string
+}
+
+type RelationShip struct {
+	SColumn *Column
+	TColumn *Column
+	Type    string
 }
 
 type Record struct {
 	LRelation    *Relation
 	RRelation    *Relation
-	RelationShip map[Column]Column
-	Alias        string
-	Type         string
+	LSubQuery    *Record
+	RSubQuery    *Record
+	RelationShip map[string]*RelationShip
+}
+
+func (records *Record) GetRelationShip() map[string]*RelationShip {
+	r := records.RelationShip
+	if records.LSubQuery != nil {
+		r = MergeMap(r, records.LSubQuery.GetRelationShip())
+	}
+	if records.RSubQuery != nil {
+		r = MergeMap(r, records.RSubQuery.GetRelationShip())
+	}
+	return r
 }
 
 // 返回左右表间的关系，所以主体有两个表，外加关系，不过如果返回的是一个子查询，理论上其也会有个别名
@@ -59,25 +85,14 @@ func parseJoinClause(node *pg_query.Node, aliasMap map[string]*Relation) *Record
 
 	j := node.GetJoinExpr()
 	var lRelation, rRelation *Relation
+	var lSubQuery, rSubQuery *Record
 
 	// 优先遍历内层JOIN
 	if j.GetLarg().GetJoinExpr() != nil {
-		lnode := parseJoinClause(j.GetLarg(), aliasMap)
-
-		lRelation = &Relation{
-			RelName: "", // ???
-			Schema:  "", // ???
-			Alias:   lnode.Alias,
-		}
+		lSubQuery = parseJoinClause(j.GetLarg(), aliasMap)
 	}
 	if j.GetRarg().GetJoinExpr() != nil {
-		rnode := parseJoinClause(j.GetRarg(), aliasMap)
-
-		rRelation = &Relation{
-			RelName: "",
-			Schema:  "",
-			Alias:   rnode.Alias,
-		}
+		rSubQuery = parseJoinClause(j.GetRarg(), aliasMap)
 	}
 	if j.GetLarg().GetRangeVar() != nil {
 		lnode := j.GetLarg().GetRangeVar()
@@ -103,39 +118,37 @@ func parseJoinClause(node *pg_query.Node, aliasMap map[string]*Relation) *Record
 
 		aliasMap[rRelation.Alias] = rRelation
 	}
-
+	// 记录关系
 	relationShip := parseQuals(j, aliasMap)
 
-	r := &Record{
+	return &Record{
 		LRelation:    lRelation,
 		RRelation:    rRelation,
+		LSubQuery:    lSubQuery,
+		RSubQuery:    rSubQuery,
 		RelationShip: relationShip,
-		Type:         j.GetJointype().String(),
-		Alias:        j.GetAlias().GetAliasname(),
 	}
-
-	return r
 }
 
-func parseQuals(node *pg_query.JoinExpr, aliasMap map[string]*Relation) map[Column]Column {
+func parseQuals(node *pg_query.JoinExpr, aliasMap map[string]*Relation) map[string]*RelationShip {
 	if node.GetQuals() == nil {
 		return nil
 	}
 
-	relationShip := make(map[Column]Column)
+	var relationShip map[string]*RelationShip
 
 	if node.GetQuals().GetAExpr() != nil {
-		relationShip = parseAExpr(node.GetQuals().GetAExpr(), aliasMap)
+		relationShip = parseAExpr(node.GetQuals().GetAExpr(), node.GetJointype(), aliasMap)
 	} else if node.GetQuals().GetBoolExpr() != nil {
-		relationShip = parseBoolExpr(node.GetQuals().GetBoolExpr(), aliasMap)
+		relationShip = parseBoolExpr(node.GetQuals().GetBoolExpr(), node.GetJointype(), aliasMap)
 	}
 	// ...
 
 	return relationShip
 }
 
-func MergeMap(mObj ...map[Column]Column) map[Column]Column {
-	newObj := map[Column]Column{}
+func MergeMap(mObj ...map[string]*RelationShip) map[string]*RelationShip {
+	newObj := make(map[string]*RelationShip)
 	for _, m := range mObj {
 		for k, v := range m {
 			newObj[k] = v
@@ -144,24 +157,26 @@ func MergeMap(mObj ...map[Column]Column) map[Column]Column {
 	return newObj
 }
 
-func parseBoolExpr(expr *pg_query.BoolExpr, aliasMap map[string]*Relation) map[Column]Column {
-	m := map[Column]Column{}
+func parseBoolExpr(expr *pg_query.BoolExpr, joinType pg_query.JoinType, aliasMap map[string]*Relation) map[string]*RelationShip {
+	m := make(map[string]*RelationShip)
 	for _, v := range expr.GetArgs() {
-		m = MergeMap(m, parseAExpr(v.GetAExpr(), aliasMap))
+		m = MergeMap(m, parseAExpr(v.GetAExpr(), joinType, aliasMap))
 	}
 	return m
 }
 
-func parseAExpr(expr *pg_query.A_Expr, aliasMap map[string]*Relation) map[Column]Column {
+func parseAExpr(expr *pg_query.A_Expr, joinType pg_query.JoinType, aliasMap map[string]*Relation) map[string]*RelationShip {
+	m := make(map[string]*RelationShip)
+
 	l := expr.GetLexpr()
 	r := expr.GetRexpr()
 
-	var key, value Column
+	relationship := &RelationShip{}
 
 	if len(l.GetColumnRef().GetFields()) == 2 {
 		rel := aliasMap[l.GetColumnRef().GetFields()[0].GetString_().GetStr()]
 
-		key = Column{
+		relationship.SColumn = &Column{
 			Schema:  rel.Schema,
 			RelName: rel.RelName,
 			Field:   l.GetColumnRef().GetFields()[1].GetString_().GetStr(),
@@ -171,14 +186,22 @@ func parseAExpr(expr *pg_query.A_Expr, aliasMap map[string]*Relation) map[Column
 	if len(r.GetColumnRef().GetFields()) == 2 {
 		rel := aliasMap[r.GetColumnRef().GetFields()[0].GetString_().GetStr()]
 
-		value = Column{
+		relationship.TColumn = &Column{
 			Schema:  rel.Schema,
 			RelName: rel.RelName,
 			Field:   r.GetColumnRef().GetFields()[1].GetString_().GetStr(),
 		}
 	}
 
-	return map[Column]Column{
-		key: value,
-	}
+	relationship.Type = joinType.String()
+	// checksum
+	key := Hash(relationship)
+
+	m[key] = relationship
+	return m
+}
+
+func Hash(s *RelationShip) string {
+	data, _ := json.Marshal(s)
+	return fmt.Sprintf("%x", md5.Sum(data))
 }
