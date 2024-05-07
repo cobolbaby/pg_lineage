d4a43ec8b52af9f68295aaa42626c9b43c8fa395
Cobolbaby <Zhang.Xing-Long@inventec.com>
2024-05-07 13:37:52 +0800

    style: 清理不相关测试代码

--- example/depgraph/main.go
+++ example/depgraph/main.go
@@ -6,8 +6,6 @@ import (
 	"time"
 
 	"pg_lineage/pkg/depgraph"
-
-	"github.com/mitchellh/copystructure"
 )
 
 type Record struct {
@@ -101,60 +99,4 @@ func main() {
 	// 1: dw.test_table5
 	// 2: dw.test_table6, dw.test_table7
 
-	// 深，浅拷贝
-	// map[string]depgraph.Node
-	src1 := g.GetNodes()
-	dst11 := deepCopyWithPointer(src1)
-	dst12 := deepCopyWithPointerV2(src1)
-	fmt.Printf("deepCopyWithPointer GetNodes, src1: %+v, dst11: %+v, dst12: %+v\n", src1, dst11, dst12)
-
-	// map[string]map[string]struct{}
-	src2 := g.GetRelationships()
-	dst21 := deepCopyWithStruc(src2)
-	dst22 := deepCopyWithStrucV2(src2)
-	fmt.Printf("deepCopyWithStruc GetRelationships, src2: %+v, dst21: %+v, dst22: %+v\n", src2, dst21, dst22)
-
-	// &Graph{}
-	gc, err := copystructure.Copy(g)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("copystructure.Copy(g), g: %+v, gc: %+v\n", g, gc.(*depgraph.Graph))
-	gc2 := new(depgraph.Graph)
-	*gc2 = *g
-	fmt.Printf("new(depgraph.Graph) *gc2 = *g, g: %+v, gc: %+v\n", g, gc2)
-
-}
-
-// 如果 depgraph.Node 为指针，以下方法无法做到深拷贝
-func deepCopyWithPointer(src map[string]depgraph.Node) map[string]depgraph.Node {
-	dst := make(map[string]depgraph.Node)
-	for k, v := range src {
-		dst[k] = v
-	}
-	return dst
-}
-
-// 如果 depgraph.Node 为指针，以下方法可以做到深拷贝
-func deepCopyWithPointerV2(src map[string]depgraph.Node) map[string]depgraph.Node {
-	dst, _ := copystructure.Copy(src)
-	return dst.(map[string]depgraph.Node)
-}
-
-// 可以做到深拷贝，主要原因是在与 struct{} 为非指针类型
-func deepCopyWithStruc(src map[string]map[string]struct{}) map[string]map[string]struct{} {
-	dst := make(map[string]map[string]struct{})
-	for k, v := range src {
-		tt := make(map[string]struct{}, len(v))
-		for kk, vv := range v {
-			tt[kk] = vv
-		}
-		dst[k] = tt
-	}
-	return dst
-}
-
-func deepCopyWithStrucV2(src map[string]map[string]struct{}) map[string]map[string]struct{} {
-	dst, _ := copystructure.Copy(src)
-	return dst.(map[string]map[string]struct{})
 }
